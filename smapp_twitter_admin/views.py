b7897eca0e26f495bc358dceb0df602946a2428e
Jonathan Ronen <yablee@gmail.com>
2014-06-20 18:13:08 -0400

    Add bootstrap, simple navbar layout

--- smapp_twitter_admin/views.py
+++ smapp_twitter_admin/views.py
@@ -1,9 +1,26 @@
 from smapp_twitter_admin import app
-from flask import session, render_template, redirect
+from smapp_twitter_admin.oauth_module import current_user
+from flask import session, render_template, redirect, request
+
+@app.before_request
+def user_login_check():
+    if not request.path in ['/', '/login', '/oauthorized']:
+        print current_user()
+        if current_user():
+            return
+        return redirect('/')
 
 @app.route('/')
 def welcome_view():
     if 'twitter_oauth' in session:
-        return redirect('/admin')
+        return redirect('/dashboard')
     else:
         return render_template('welcome.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/filter-criteria')
+def filter_criteria_index():
+    return render_template('filter-criteria/index.html')
