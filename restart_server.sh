d5c180da06ec66efcc569ce3ce9c01c474658a06
yvan <yvanscher@gmail.com>
2016-05-21 23:22:19 -0400

    changed the bind addr to be 127.0.0.1:8000 and not some unix socket

--- restart_server.sh
+++ restart_server.sh
@@ -1,2 +1,2 @@
 killall gunicorn
-/home/yvan/miniconda3/envs/smapp-twitter-admin/bin/gunicorn smapp_twitter_admin:app -b unix:/tmp/gunicorn_flask.sock -w 4 -t 120 -D --access-logfile logs/gunicorn/gunicorn_access_log.log --error-logfile logs/gunicorn/gunicorn_error_log.log
+/home/yvan/miniconda3/envs/smapp-twitter-admin/bin/gunicorn smapp_twitter_admin:app -b 127.0.0.1:8000 -w 4 -t 120 -D --access-logfile logs/gunicorn/gunicorn_access_log.log --error-logfile logs/gunicorn/gunicorn_error_log.log
